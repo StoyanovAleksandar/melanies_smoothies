a52e0dc8909cf9e484a1625b291024656cff7c28
StoyanovAleksandar <aleksandar.stoyanov@blankfactor.com>
2025-03-07 17:43:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,7 @@ st.write(
 name_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your smoothie will be:", name_order)
 
-#session = get_active_session()
-cnx = st.connection("snowflake")
-session = cnx.session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'));
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -49,3 +47,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+cnx = st.connection("snowflake")
+session = cnx.session();
